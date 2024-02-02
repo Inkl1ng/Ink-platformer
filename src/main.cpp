f972e413a9f7fde16eee54707c81cd42b6359f05
Inkl1ng <scottbot2018@gmail.com>
2024-02-01 18:14:15 -0800

    Rewrite

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,12 @@
+#include <iostream>
 #include "Game.hpp"
 
-#include <iostream>
-
-int main()
-{
-#ifndef NDEBUG
-    std::cout << "*******************\n";
-    std::cout << "*** DEBUG BUILD ***\n";
-    std::cout << "*******************\n";
-#endif
+int main() {
+    #ifndef NDEBUG
+        std::cout << "*******************\n";
+        std::cout << "*** DEBUG BUILD ***\n";
+        std::cout << "*******************\n";
+    #endif
 
     Game game {};
     game.run();
